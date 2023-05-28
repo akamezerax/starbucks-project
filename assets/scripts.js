40c0909738da97c8a419d21be05ae6e514ebb831
akamezerax <joaomarcosdasilva1@gmail.com>
2023-05-28 12:45:33 -0300

    first commit

---
+++ assets/scripts.js
@@ -0,0 +1,10 @@
+let image = document.querySelector(".starbucks")
+let circle = document.querySelector(".circle")
+
+function imageReplace(adress) {
+    image.src = adress
+}
+
+function colorReplace(color) {
+    circle.style.background = color
+}
